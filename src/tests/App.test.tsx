7233c9cece028c79aa96e5df564b8d254af2e9e8
FabioRosado <fabiorosado@outlook.com>
2022-05-23 21:36:53 +0200

    Fix App test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,7 +3,18 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  // Confirm that the prompt was loaded successfully
+  const prompt = screen.getByPlaceholderText("Say something...");
+  expect(prompt).toBeInTheDocument();
+
+  // Confirm that the conversation component loaded successfully
+  expect(container.getElementsByClassName("conversation").length).toBe(1);
+
+  // Confirm that connection settings were loaded successfully
+  const submitButton = screen.getByDisplayValue("Connect");
+  expect(submitButton).toBeInTheDocument();
+
+  const protocol = screen.getByText("http://");
+  expect(protocol).toBeInTheDocument();
 });
