cfa44a918dcdcf26c6612917094be8930a89fe0f
FabioRosado <fabiorosado@outlook.com>
2022-06-19 13:34:38 +0100

    Small improvements to the message bubble

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -6,32 +6,68 @@ interface MessageProps {
   message: MessageType;
 }
 
+// export const Message = ({ message }: MessageProps): React.ReactElement => {
+//   const getTime = (timestamp: Date): string => {
+//     return timestamp.toTimeString().replace(/.*(\d{2}:\d{2}):\d{2}.*/, "$1");
+//   };
+//   const user = message.user !== "opsdroid" ? "user" : message.user;
+//   return (
+//     <div className="message">
+//       <div className="message-header">
+//       </div>
+//       {message.image && (
+//         <li className={user}>
+//           <a className="image-url" href={message.text}>
+//             {message.text}
+//           </a>
+//           <li
+//             className={`${user} image`}
+//             style={{ backgroundImage: `url(${message.image})` }}
+//           />
+//         </li>
+//       )}
+//       {message.text != message.image && (
+//         <li className={user}>{message.text}</li>
+//       )}
+//       <li className="clearfix" />
+//       {message.user != "info" && (
+//         <li className={`${user} timestamp`}>{getTime(message.timestamp)}</li>
+//       )}
+//       <li className="clearfix" />
+//     </div>
+//   );
+// };
+
 export const Message = ({ message }: MessageProps): React.ReactElement => {
   const getTime = (timestamp: Date): string => {
     return timestamp.toTimeString().replace(/.*(\d{2}:\d{2}):\d{2}.*/, "$1");
   };
   const user = message.user !== "opsdroid" ? "user" : message.user;
   return (
-    <div className="message">
-      {message.image && (
+    <div className="message-section">
+      <div className={`${user} message`}>
         <li className={user}>
-          <a className="image-url" href={message.text}>
-            {message.text}
-          </a>
-          <li
-            className={`${user} image`}
-            style={{ backgroundImage: `url(${message.image})` }}
-          />
+          <p
+            className={`username ${user !== "opsdroid" ? "invert" : "normal"}`}
+          >
+            {user}{" "}
+            <span className="timestamp">{getTime(message.timestamp)}</span>
+          </p>
+          {message.image && (
+            <p>
+              <a className="image-url" href={message.text}>
+                {message.text}
+              </a>
+              <p
+                className={`${user} image`}
+                style={{ backgroundImage: `url(${message.image})` }}
+              />
+            </p>
+          )}
+          {message.text != message.image && <p>{message.text}</p>}
         </li>
-      )}
-      {message.text != message.image && (
-        <li className={user}>{message.text}</li>
-      )}
-      <li className="clearfix" />
-      {message.user != "info" && (
-        <li className={`${user} timestamp`}>{getTime(message.timestamp)}</li>
-      )}
-      <li className="clearfix" />
+        <li className="clearfix" />
+      </div>
     </div>
   );
 };
